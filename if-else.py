089a9ecfe3e3794c2511c65193eaecad00c8d104
Bantupalli Bhaskara Rao <127121617+Bhaskar-Rao-1@users.noreply.github.com>
2023-08-31 19:01:28 +0530

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,11 @@
+x=input("what's your name? ")
+if x=="ram":
+    print ("madhurawasa")
+elif x=="jnani":
+    print("vizag")
+elif x=="harsha":
+    print("s Kota")
+elif x=="gani":
+    print("Purna market")
+else:
+    print("who?")
---
+++ match.py
@@ -0,0 +1,13 @@
+x=input("what's your name? ")
+
+match x:
+    case "ram":
+        print ("madhurawada")
+    case "gani":
+        print ("Purna market")
+    case "Harsha":
+        print("s Kota")
+    case "jnani":
+        print("vizag")
+    case _:
+        print("who?")
