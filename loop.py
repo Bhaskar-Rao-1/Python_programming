16aa0d4ec7d5655e2a0093c05d8247d7d06ddf55
Bantupalli Bhaskara Rao <127121617+Bhaskar-Rao-1@users.noreply.github.com>
2023-09-01 18:18:03 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,10 @@
+while True:
+    x=int(input("what number? "))
+    if x>0:
+        break
+        
+        
+        
+
+for _ in range(x):
+   print("hello")
---
+++ while.py
@@ -0,0 +1,7 @@
+i=0
+while i<3:
+    print("hi")
+    i+=1
+    
+    
+print("hi\n"*3)
