4b160bacb23f6804ff3ee66f1def116472b41024
Bantupalli Bhaskara Rao <127121617+Bhaskar-Rao-1@users.noreply.github.com>
2023-09-02 18:20:30 +0530

    Add files via upload

---
+++ dist_loop.py
@@ -0,0 +1,3 @@
+students={"ram":"madhurawada","Jani":"vizag","Harsha":"s Kota"}
+for student in students:
+    print(student, students[student] , sep=":")
---
+++ list_loop.py
@@ -0,0 +1,8 @@
+students=["ram","raju","Harsha"]
+for i in students:
+    print(i)
+    #or
+for i in range(len(students)):
+    print(students[i])
+    
+    
