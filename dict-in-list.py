c75d2d7d9bc796f299bb7a56d2fb12cad741554e
Bantupalli Bhaskara Rao <127121617+Bhaskar-Rao-1@users.noreply.github.com>
2023-09-03 19:03:23 +0530

    Add files via upload

---
+++ dict-in-list.py
@@ -0,0 +1,6 @@
+students=[
+{"name":"ram","house":"vizag","patrous":"vidi"},
+{"name":"jnani","house":"madhurawada","patrous":"Maddi"}
+]
+for student in students:
+    print (student["name"], student["house"],sep=",")
