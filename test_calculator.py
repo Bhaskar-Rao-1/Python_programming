0733161336af3057d1a141f6b64976cd829e1049
Bantupalli Bhaskara Rao <127121617+Bhaskar-Rao-1@users.noreply.github.com>
2023-09-10 09:58:34 +0530

    Add files via upload

---
+++ test_calculator.py
@@ -0,0 +1,28 @@
+def main():
+    test_calculator()
+def square(n):
+    return n+n
+def test_calculator():
+    try:
+        assert square(2)==4
+    except AssertionError:
+        print("2 square is not 4")
+    try:
+        assert square(3)==9
+    except AssertionError:
+        print("3 square is not 9")
+    try:
+        assert square(-2)==4
+    except AssertionError:
+        print("-2 square is not 4")
+    try:
+        assert square(+3)==9
+    except AssertionError:
+        print("-3 square is not 9")
+    try:
+        assert square(0)==0
+    except AssertionError:
+        print("0 square is not 0")
+if __name__=="__main__":
+    main()
+     
---
+++ tip.py
@@ -0,0 +1,4 @@
+w=int(input("how much bill?  "))
+y=int(input("how much percentage of tip you want to give.  "))
+x=(w*y)/100
+print("leave",x)
